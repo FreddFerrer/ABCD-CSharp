37508ccac72bbfb9256f0be44134abed8a2f2bc5
FreddyWhitDoubleD <ferreirafreddy39@gmail.com>
2022-05-05 02:41:56 -0300

    examen

---
+++ Periodos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABCexamen
+{
+    internal class Periodos
+    {
+
+        
+
+        public static void ImprimirOriginales(DateTime[] fechas)
+        {
+            /*for(int i = 0; i < fechas.Length; i++)
+            {
+                Console.WriteLine("El Periodo 1 va desde: " + fechas[i].ToLongDateString() + "    hasta: " + fechas[i+1].ToLongDateString());
+            }*/
+
+            Console.WriteLine("El Periodo A va desde: " + fechas[0].ToLongDateString() + "    hasta: " + fechas[1].ToLongDateString());
+            Console.WriteLine("El Periodo B va desde: " + fechas[2].ToLongDateString() + "    hasta: " + fechas[3].ToLongDateString());
+            Console.WriteLine("El Periodo C va desde: " + fechas[4].ToLongDateString() + "    hasta: " + fechas[5].ToLongDateString());
+            Console.WriteLine("El Periodo D va desde: " + fechas[6].ToLongDateString() + "    hasta: " + fechas[7].ToLongDateString());
+
+        }
+
+        
+
+        
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ABCexamen
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            DateTime[] dates = { new DateTime (2022, 01, 05), new DateTime (2022, 02, 20), //46 dias
+                             new DateTime (2022, 01, 28), new DateTime (2022, 03, 15),    //46 dias
+                             new DateTime (2022, 01, 07), new DateTime (2022, 01, 25),   //18 dias
+                             new DateTime (2022, 03, 17), new DateTime (2022, 04, 04)   //18 dias
+            };
+
+            Periodos.ImprimirOriginales(dates);
+
+            
+
+            
+        }
+    }
+}
